dcc73f24e3e84ec7110912d7e1041623b16a8644
mahi <bharathiinstitute1@gmail.com>
2025-08-20 15:47:54 +0530

    krish

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,43 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MainApp());
 }
-
+kanna
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Your Text Here', style: TextStyle(fontSize: 24)),
+              SizedBox(height: 24),
+              ...List.generate(6, (index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 8,
+                        offset: Offset(2, 4),
+                      ),
+                    ],
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0, // Shadow handled by Container
+                    ),
+                    child: Text('Button ${index + 1}'),
+                  ),
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
